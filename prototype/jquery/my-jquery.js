7aacf47617edb108644cb98c4481a71ba320dd2c
赵安东 <zhaoandong4213@sina.com>
2018-08-28 23:24:00 +0800

    ...

--- prototype/jquery/my-jquery.js
+++ prototype/jquery/my-jquery.js
@@ -2,6 +2,7 @@
 
     var jQuery = function(selector) {
         return new jQuery.fn.init(selector)
+        // return new kk(selector)
     }
 
     jQuery.fn = jQuery.prototype = {
@@ -19,10 +20,11 @@
     // console.log(jQuery.fn)
     
     var init = jQuery.fn.init =function(selector) {
+    // var init = kk =function(selector) {
         var slice = Array.prototype.slice;
         var dom = slice.call(document.querySelectorAll(selector))
-
-        var i, len = dom ? dom.length : 0;
+        console.log(this)
+        // var i, len = dom ? dom.length : 0;
         for(i = 0; i < len; i++) {
             this[i] = dom[i]
         }
@@ -37,8 +39,9 @@
 })(window)
 
 // 通过$函数初始化jQuery实例的时候
+// 那么问题来了，jQuery.fn 中的方法为什么不直接定义在 init 的prototype上，而要定义在 jQuery 的原型对象上？
 
-
+/* 其实，这样做的目的是为了提高 jQuery 的查询效率，如果直接定义在 init 的 prototype 对象上，那么每执行一次查询，就会在内存中创建这样一个庞大的prototype对象，而如果把这个对象定义在jQuery的prototype上，在jQuery加载时，这个对象就会被初始化并一直存在于内存中，以后每次执行 $() 时，只需要将init中的prototype指向这个对象就可以了，而不用每次都去创建一遍相同的对象。 */
 
 
 
